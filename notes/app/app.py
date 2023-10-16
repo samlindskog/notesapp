1c1437c3a47d881f8fcfa7cbc263845487ad21df
Samuel Lindskog <lindskogsamuel@yahoo.com>
2023-10-16 14:07:14 -0600

    consolidated functionality into App.route()

--- notes/app/app.py
+++ notes/app/app.py
@@ -1,5 +1,7 @@
 import logging
 import re
+import pdb
+from urllib.parse import parse_qs
 from app.responses import rstart404_html, rstart400_html, rbody
 
 '''
@@ -12,25 +14,41 @@ https://asgi.readthedocs.io/en/latest/specs/www.html for more details.
 '''
 class App():
     #[scope_key, ...]
-    _scope_params = []
+    _route_scope_keys = []
     #[[path, [scope_value, ...], callback], ...]
     _routes = []
 
+    def _inject_scope(self, scope, params):
+        for key in params.keys():
+            scope[key] = params[key]
+
     async def run(self, scope, recieve, send):
-        current_scope_values = self._scope_values(scope) or []
         try:
+            current_scope_values = [scope[k] for k in self._route_scope_keys]
+            current_scope_values.sort()
             for route in self._routes:
                 pattern = re.compile(route[0])
                 match = pattern.match(scope["path"])
                 if not match:
                     continue
                 elif not current_scope_values:
+                    self._inject_scope(
+                            scope, {
+                            "group": list(match.groups()),
+                            "qs_args": self._querystring_argparser(scope["query_string"], route[2])
+                            })
                     #run callback with current request parameters
-                    await route[2](scope, recieve, send)
+                    await route[3](scope, recieve, send)
                     break
-                for param in current_scope_values:
-                    if route[1] == param:
-                        await route[2](scope, recieve, send)
+                route_scope_values = [v for k,v in route[1].items()]
+                route_scope_values.sort()
+                if route_scope_values == current_scope_values:
+                    self._inject_scope(
+                            scope, {
+                            "group": list(match.groups()),
+                            "qs_args": self._querystring_argparser(scope["query_string"], route[2])
+                            })
+                    await route[3](scope, recieve, send)
                 else:
                     continue
                 break
@@ -42,23 +60,82 @@ class App():
             logging.debug(str(e))
             await send(rstart400_html)
             await send(rbody("Error 400: bad request :(".encode('utf-8')))
+    
+    #parse querystring to dictionary and decode b"key":[b"value"] pairs
+    def _querystring_decode(self, query_string):
+        query_dictionary = parse_qs(query_string, strict_parsing=True, errors="strict")
+        return {k.decode("utf-8"): v[0].decode("utf-8") for k, v in query_dictionary.items()}
 
-  
-    #params must contain the same keys for each invocation
+    '''
+    query_string is a percent encoded querystring, and target_args is a dictionary comprised
+    of argument keywords with their respective type, i.e. {arg: type}. return a list of 
+    parameters present in querystring which match target_args.
+
+    will ignore empty or extraneous query params
+
+    throws exceptions when params matching target_args are invalid, or if querystring itself
+    is invalid
+    '''
+    def _querystring_argparser(self, query_string, target_args):
+        query_dictionary = self._querystring_decode(query_string)
+        args = [[k,v, target_args[k]] for k,v in query_dictionary.items() if k in target_args.keys()]
+        valid_args = {}
+        for arg in args:
+            key = arg[0]
+            value = arg[1]
+            t=arg[2]
+            if not value:
+                raise Exception("invalid querystring: empty parameter")
+            match t:
+                case "string":
+                    valid_args[key] = value
+                case "int":
+                    if len(value) > 2:
+                        raise Exception("invalid querystring: value of type int exceeds length 2")
+                    valid_args[key] = int(value)
+                case "bool":
+                    if value == "true":
+                        valid_args[key] = True
+                    elif value == "false":
+                        valid_args[key] = False
+                    else:
+                        raise Exception("invalid querystring: value of type bool invalid")
+                case _:
+                    raise Exception("invalid target_args: type unsupported")
+        return valid_args
+
+    '''
+    route is a decorator which runs decorated function when a request
+    matches specified conditions
+
+    path is regex pattern matched against scope["path"]. All groups are
+    matched and passed as a list into scope["group"]
+
+    params, of the form {"key": "value", ...}, is used to match requests
+    whose scope["key"]=="value". Must contain the same keys for each
+    invocation of App.route(). All filter params being specified by their
+    keys in each invocation enhances readability, and mitigates phantom
+    matches at runtime
+
+    qs_args, of the form {"key":"type", ...} specifies how to
+    parse query string parameters into a dictionary, with key matched
+    against querystring keys, and type used to convert querystring
+    values. The resulting dictionary is passed into scope["qs_args"]
+    '''      
     @classmethod
-    def route(cls, path, params={}):
+    def route(cls, path, scope_params={}, qs_args={}):
         def wrapper(fn):
-            param_items = params.items()
-            param_values = [v for k,v in param_items].sort()
-            #list of scope keys
-            cls._scope_params = [k for k,v in param_items]
-            #sorted list of scope values from params dictionary
-            cls._routes.append([path, param_values, fn])
+            scope_items = scope_params.items()
+            scope_keys = [k for k,v in scope_params.items()]
+            scope_keys.sort()
+            if not cls._routes:
+                cls._route_scope_keys = scope_keys
+            #test this plz
+            elif cls._routes[-1] and scope_keys != cls._route_scope_keys:
+                raise Exception("App.route param keys differ")
+
+            #list of routes
+            cls._routes.append([path, scope_params, qs_args, fn])
             return fn
         return wrapper
-
-    def _scope_values(self, scope):
-        current_request_params = [scope[key] for key in self._scope_params]
-        #sorted list of scope values with keys from self._scope_params dictionary
-        return current_request_params.sort()
 
--- notes/app/routes.py
+++ notes/app/routes.py
@@ -1,14 +1,8 @@
-import string
-from typing import ByteString
-from urllib.parse import parse_qs
-import re
 import pdb
-import logging
 
 from app.app import App
 from app.responses import rbody, rstart200_json
 
-#loop through and return full body of request
 async def read_body(receive):
     body = b''
     more_body = True
@@ -18,64 +12,15 @@ async def read_body(receive):
         more_body = message.get('more_body', False)
     return body
 
-#parse querystring to dictionary and decode b"key":[b"value"] pairs
-def _querystring_decode(query_string):
-    query_dictionary = parse_qs(query_string, strict_parsing=True, errors="strict")
-    return {k.decode("utf-8"): v[0].decode("utf-8") for k, v in query_dictionary.items()}
-
-'''
-query_string is a percent encoded querystring, and target_args is a dictionary comprised
-of argument keywords with their respective type, i.e. {arg: type}. return a list of 
-parameters present in querystring which match target_args.
-
-will ignore empty or extraneous query params
-
-throws exceptions when params matching target_args are invalid, or if querystring itself
-is invalid
-'''
-def _querystring_argparser(query_string, target_args):
-    query_dictionary = _querystring_decode(query_string)
-    args = [[k,v, target_args[k]] for k,v in query_dictionary.items() if k in target_args.keys()]
-    valid_args = {}
-    for arg in args:
-        key = arg[0]
-        value = arg[1]
-        t=arg[2]
-        if not value:
-            raise Exception("invalid querystring: empty parameter")
-        match t:
-            case "string":
-                valid_args[key] = value
-            case "int":
-                if len(value) > 2:
-                    raise Exception("invalid querystring: value of type int exceeds length 2")
-                valid_args[key] = int(value)
-            case "bool":
-                if value == "true":
-                    valid_args[key] = True
-                elif value == "false":
-                    valid_args[key] = False
-                else:
-                    raise Exception("invalid querystring: value of type bool invalid")
-            case _:
-                raise Exception("invalid target_args: type unsupported")
-    return valid_args
-
-@App.route(r"/profiles/.+/")
+@App.route(
+        r"/profiles/(.+)/",
+        scope_params={"method": "GET"},
+        qs_args={"orderby": "string","limit": "int","offset": "int","desc": "bool"})
 async def run_profiles(scope, recieve, send):
-    #less clunky then passing previous match through parameters
-    pattern = re.compile(pattern=r"/profiles/(.+)/")
-    pattern_match = pattern.search(scope["path"])
-    assert isinstance(pattern_match, re.Match), "pattern match null"
-    
-    request_body = await read_body(recieve)
-    resource_path = pattern_match.group(1)
-    querystring_args = _querystring_argparser(
-            scope["query_string"], {"orderby": "string","limit": "int","offset": "int","desc": "bool"})
-
-
-
+    resource_path = scope["group"][0]
+    querystring_args = scope["qs_args"]
     profiles = scope['state']['profiles']
+    body = await read_body(recieve)
     
     if resource_path:
         async with profiles() as p:
@@ -83,6 +28,5 @@ async def run_profiles(scope, recieve, send):
             await send(rstart200_json)
             await send(rbody(response_body))
     else:
-        print("value error")
         raise Exception("send did not run")
 
